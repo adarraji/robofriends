aaa74c0529c0c521a2bd6b84e0a52b42410dba94
adarraji <a.darraji@outlook.com>
2022-03-26 18:48:44 -0400

    Added ErrorBoundry component

---
+++ src/components/ErrorBoundry.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class ErrorBoundry extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hasError: false;
+        }
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return <h1>Oooops. That is not good</h1>
+        }
+        return this.props.children;
+    }
+}
+
+export default ErrorBoundry;
