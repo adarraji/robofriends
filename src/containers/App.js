816c1cf925b908afa83f8fcada4e55f43fd5f0f9
adarraji <a.darraji@outlook.com>
2022-03-26 19:02:54 -0400

    import ErrorBoundry, add to CardList to catch errors

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,9 +3,7 @@ import "./App.css";
 import CardList from "../components/CardList";
 import Scroll from "../components/Scroll";
 import SearchBox from "../components/SearchBox"
-
-
-
+import ErrorBoundry from "../components/ErrorBoundry";
 
 class App extends Component {
     constructor() {
@@ -35,9 +33,11 @@ class App extends Component {
                     <h1 className="f1">RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
                     <Scroll>
-                        <CardList robots={filteredRobots} />
+                        <ErrorBoundry>
+                            <CardList robots={filteredRobots} />
+                        </ErrorBoundry>
                     </Scroll>
-                </div>
+                </div >
             )
     }
 }
