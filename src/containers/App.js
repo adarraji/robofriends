bf64a75c6563abd0f40203964a5d91440f7d6c85
adarraji <a.darraji@outlook.com>
2022-10-19 21:26:27 -0400

    changed to func. comp. and hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import "./App.css";
 import CardList from "../components/CardList";
 import Scroll from "../components/Scroll";
@@ -7,48 +7,36 @@ import SearchBox from "../components/SearchBox"
 import ErrorBoundry from "../components/ErrorBoundry";
 import { setSearchField, requestrobots } from "../actions";
 
-const mapStateToProps = state => {
-    return {
-        searchField: state.searchRobots.searchField,
-        robots: state.requestRobots.robots,
-        isPending: state.requestRobots.isPending,
-        error: state.requestRobots.error,
+const App = () => {
+    const searchField = useSelector(state => state.searchRobots.searchField);
+    const robots = useSelector(state => state.requestRobots.robots);
+    const isPending = useSelector(state => state.requestRobots.isPending);
+
+    const dispatch = useDispatch();
+
+    const onSearchChange = (e) => {
+        dispatch(setSearchField(e.target.value));
     }
+
+    useEffect(() => {
+        dispatch(requestrobots());
+    }, []);
+
+    const filteredRobots = robots.filter(robot => robot.name.toLowerCase().includes(searchField.toLowerCase()));
+
+    return isPending ? <h1>Loading</h1> :
+        (
+
+            <div className="tc" >
+                <h1 className="f1">RoboFriends</h1>
+                <SearchBox searchChange={onSearchChange} />
+                <Scroll>
+                    <ErrorBoundry>
+                        <CardList robots={filteredRobots} />
+                    </ErrorBoundry>
+                </Scroll>
+            </div >
+        )
 }
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
-        onRequestRobots: () => dispatch(requestrobots())
-    }
-}
-
-class App extends Component {
-
-    componentDidMount() {
-        this.props.onRequestRobots();
-    }
-
-    render() {
-        console.log("test");
-        const { searchField, onSearchChange, robots, isPending } = this.props;
-        const filteredRobots = robots.filter(robot => robot.name.toLowerCase().includes(searchField.toLowerCase()));
-
-        return isPending ? <h1>Loading</h1> :
-            (
-
-                <div className="tc" >
-                    <h1 className="f1">RoboFriends</h1>
-                    <SearchBox searchChange={onSearchChange} />
-                    <Scroll>
-                        <ErrorBoundry>
-                            <CardList robots={filteredRobots} />
-                        </ErrorBoundry>
-                    </Scroll>
-                </div >
-            )
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
-
+export default App; 
