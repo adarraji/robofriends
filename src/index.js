45179c0332690a8a95f30a9181445ec755e7bd05
adarraji <a.darraji@outlook.com>
2022-10-27 16:29:20 -0400

    removed redux logger

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, combineReducers } from "redux";
-import { createLogger } from "redux-logger";
+// import { createLogger } from "redux-logger";
 import thunkMiddleware from "redux-thunk";
 import './index.css';
 import reportWebVitals from './reportWebVitals';
@@ -10,7 +10,7 @@ import "tachyons";
 import { searchRobots, requestRobots } from "./reducers"
 import App from "./containers/App";
 
-const logger = createLogger();
+// const logger = createLogger();
 const rootReducer = combineReducers({ searchRobots, requestRobots })
 // const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
 const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
