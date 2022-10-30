45f7fe98d57603f6ff7c903e1f6bf695f05d30ce
adarraji <a.darraji@outlook.com>
2022-10-30 00:23:53 +0000

    aria-lable  for accessibility

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,7 +3,9 @@ import React from "react";
 const SearchBox = ({ searchChange }) => {
     return (
         <div className="pa2">
-            <input className="pa3 ba b--green bg-lightest-blue"
+            <input
+                aria-label="Search Robots"
+                className="pa3 ba b--green bg-lightest-blue"
                 type="search"
                 placeholder="search robots"
                 onChange={searchChange}
